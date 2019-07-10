4df7df6e99e42af7085e0dae569beaef7cf118c5
David <daavidkarlos@gmail.com>
2019-07-10 13:28:03 -0300

    Ajustando página de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -59,7 +59,9 @@ export class PickAddressPage {
     this.pedido.enderecoDeEntrega = {
       id: item.id
     };
-    console.log(this.pedido);
+    this.navCtrl.push('PaymentPage', {
+      pedido: this.pedido
+    });
   }
 
 
