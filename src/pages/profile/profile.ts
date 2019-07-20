6f95668b9da49a03fedb41a23b1ee3f0da3ac179
David <daavidkarlos@gmail.com>
2019-07-20 14:11:38 -0300

    Realizando upload de foto de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -18,39 +18,43 @@ export class ProfilePage {
   cameraOn: boolean = false;
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public storage: StorageService,
-              public clienteService: ClienteService,
-              public camera: Camera) {
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService,
+    public camera: Camera) {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
-    if(localUser && localUser.email){
+    if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
-                    .subscribe(response => {
-                      this.cliente = response as ClienteDTO;
-                      this.getImageIfExist();
-                    },
-                    error => {
-                      if (error.status == 403){
-                        this.navCtrl.setRoot('HomePage');
-                      }
-                    });
+        .subscribe(response => {
+          this.cliente = response as ClienteDTO;
+          this.getImageIfExist();
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
     } else {
       this.navCtrl.setRoot('HomePage');
     }
   }
 
-  getImageIfExist(){
+  getImageIfExist() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-                  .subscribe(response => {
-                    this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-                  },
-                  error => {});
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cliente${this.cliente.id}.jpg`;
+      },
+        error => { });
   }
 
-  getCameraPicture(){
+  getCameraPicture() {
 
     this.cameraOn = true;
 
@@ -64,10 +68,24 @@ export class ProfilePage {
     this.camera.getPicture(options).then((imageData) => {
       this.picture = 'data:image/png;base64,' + imageData;
       this.cameraOn = false;
-    }, 
-     (err) => {
+    },
+      (err) => {
 
-     });
+      });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+        this.getImageIfExist();
+      },
+      error => {});
+  }
+
+  cancel(){
+    this.picture = null;
   }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -5,32 +5,46 @@ import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
 import { ResponseType } from "@angular/http";
+import { ImageUtilService } from "../image-util.service";
 
 @Injectable()
-export class ClienteService{
+export class ClienteService {
 
     constructor(public http: HttpClient,
-                public storage: StorageService){
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService) {
     }
 
-    findByEmail (email: string){
+    findByEmail(email: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
-    getImageFromBucket(id: string) : Observable<any>{
-        let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType : 'blob'});
+    getImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/cliente${id}.jpg`
+        return this.http.get(url, { responseType: 'blob' });
     }
 
-    insert (clienteDTO: ClienteDTO){
-       return this.http.post(`${API_CONFIG.baseUrl}/clientes`, 
-        clienteDTO, {
-            observe: 'response',
-            responseType: 'text'
-        });
+    insert(clienteDTO: ClienteDTO) {
+        return this.http.post(`${API_CONFIG.baseUrl}/clientes`,
+            clienteDTO, {
+                observe: 'response',
+                responseType: 'text'
+            });
     }
 
-    findById (id: string){
-       return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+    findById(id: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+    }
+
+    uploadPicture(picture) {
+        let imageBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData: FormData = new FormData();
+        formData.set('file', imageBlob, 'file.png');
+
+        return this.http.post(`${API_CONFIG.baseUrl}/clientes/picture`,
+            formData, {
+                observe: 'response',
+                responseType: 'text'
+            });
     }
 }
