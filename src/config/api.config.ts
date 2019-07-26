dfd017d56971ddbc0e3f237768aa45c270460e5d
David <daavidkarlos@gmail.com>
2019-07-25 23:36:44 -0300

    Colocando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl : "http://localhost:8080",
+   // baseUrl : "http://localhost:8080",
+    baseUrl: "https://catalogo-vendas.herokuapp.com",
     bucketBaseUrl : "https://catalogo-vendas.s3-sa-east-1.amazonaws.com"
 }
